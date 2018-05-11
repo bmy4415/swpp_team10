01acbae9f7134bb3d01d88ba1e1a8275d182851b
KimShinHwi <shinhwibf25@snu.ac.kr>
2018-05-11 21:44:49 +0900

    rename views.py (Customer -> CustomerSignUp, Store -> StoreSignUp)

--- backend/teemo/urls.py
+++ backend/teemo/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from teemo import views
 
 urlpatterns = [
-    url(r'^customer/$', views.CustomerList.as_view()),
-    url(r'^store/$', views.StoreList.as_view()),
+    url(r'^customer_sign_up/$', views.CustomerSignUp.as_view()),
+    url(r'^store_sign_up/$', views.StoreSignUp.as_view()),
 ]
--- backend/teemo/views.py
+++ backend/teemo/views.py
@@ -5,13 +5,12 @@ from django.contrib.auth.models import User
 from rest_framework.response import Response
 from rest_framework import status
 
-class CustomerList(generics.ListCreateAPIView):
+class CustomerSignUp(generics.CreateAPIView):
     """
-    List all Cusomter, or Create new Customer
+    Create new Customer
     """
-    queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
-    
+
     def perform_create(self, serializer):
         new_user = User.objects.create_user(
                                  username=serializer.validated_data['user']['username'],
@@ -25,11 +24,10 @@ class CustomerList(generics.ListCreateAPIView):
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-class StoreList(generics.ListCreateAPIView):
+class StoreSignUp(generics.CreateAPIView):
     """
     List all Store, or Create new Store
     """
-    queryset = Store.objects.all()
     serializer_class = StoreSerializer
 
     def perform_create(self, serializer):
