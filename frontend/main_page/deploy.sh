d4ffcab026aa0da1488e52c41f6c7ce36cf5b526
ene5135 <ene5135@naver.com>
2018-05-13 04:49:19 +0900

    modify deploy.sh and redirection

--- frontend/main_page/deploy.sh
+++ frontend/main_page/deploy.sh
@@ -1,2 +1,4 @@
+npm install
+npm install --save django-react-csrftoken
 npm run build
 cp -r build ../../backend/pages/templates/main_page/
--- frontend/signup_customer/deploy.sh
+++ frontend/signup_customer/deploy.sh
@@ -1,2 +1,3 @@
+npm install
 npm run build
 cp -r build ../../backend/pages/templates/signup_customer/
