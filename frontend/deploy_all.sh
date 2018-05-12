8a6d2fc6b4f20ee1cc32407c2fd320adc5488eee
bmy4415 <bmy4415@MBP085ui-MacBook-Pro.local>
2018-05-13 04:22:14 +0900

    deploy_all.sh bug fixed

--- frontend/deploy_all.sh
+++ frontend/deploy_all.sh
@@ -1,4 +1,4 @@
-cd main_page; sh deploy.sh
-cd signup_customer; sh deploy.sh
-cd signup_store; sh deploy.sh
+cd main_page; sh deploy.sh; cd ..
+cd signup_customer; sh deploy.sh; cd ..
+cd signup_store; sh deploy.sh; cd..
 # TODO add store_page, customer_page
