a888d4e36d61a200d188b60696b01932484a0c3d
bmy4415 <bmy4415@MBP085ui-MacBook-Pro.local>
2018-06-03 14:20:39 +0900

    test added

--- frontend/teemo/src/App.test.js
+++ frontend/teemo/src/App.test.js
@@ -1,9 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as actions from './store/actions.js';
+
+// export const loginPassed = (id, userType) => {
+//     return {
+//         type : "LOGIN_PASSED",
+//         id : id,
+//         userType : userType,
+//     }
+// }
+//
+// export const logout = () => {
+//     return {
+//         type : "LOGOUT",
+//     }
+// }
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+describe('actions', () => {
+	it('should create an action to customer login', () => {
+		const id = 'bmy4415';
+		const userType = 'customer';
+
+		const expectedAction = {
+			type: 'LOGIN_PASSED',
+			id,
+			userType,
+		};
+
+		expect(actions.loginPassed(id, userType)).toEqual(expectedAction);
+	});
+
+	it('should create an action to store login', () => {
+		const id = 'starbucks';
+		const userType = 'store';
+
+		const expectedAction = {
+			type: 'LOGIN_PASSED',
+			id,
+			userType,
+		};
+
+		expect(actions.loginPassed(id, userType)).toEqual(expectedAction);
+	});
+
+	it('should create an action to logout', () => {
+		const expectedAction = {
+			type: 'LOGOUT',
+		};
+
+		expect(actions.logout()).toEqual(expectedAction);
+	});
 });
